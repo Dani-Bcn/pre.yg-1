6caa07a9c9a3e8a3bd1d7a82d32deef9b6a1fe7e
Dani-Bcn <nneodani@gmail.com>
2023-12-23 20:35:55 +0100

    test

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -1,4 +1,6 @@
+import gsap from "gsap";
 import React, { useEffect } from "react";
+
 
 export default function Carousel() {
   const arrayCarousel = [
@@ -22,20 +24,38 @@ export default function Carousel() {
   ];
 
   return (
-    <main>
+    <main id="main">
       <section className="flex w-screen h-52 overflow-auto">
         {arrayCarousel.map((e, i) => {
           console.log(e.url);
           return (
-            <section key={e.url} className="flex w-screen h-52 justify-center items-center">
-              <div width={900}
-              id="divImg"
-               style={{background:`url(${e.url})`}}  >
-              <p className="w-screen  h-52 text-center flex  ">{e.text}</p>
+            <section key={e.url} className="flex h-52 justify-center items-center">
+              <div              
+              className="w-screen h-52 "
+               style={{background:`url(${e.url})`,
+               backgroundPosition:"center",
+               backgroundSize:"cover",
+               display:"flex",
+               alignItems:"center",
+               justifyContent:"center"
+               }}  >
+                 <p className="w-full h-52 backdrop-brightness-50 text-white text-center font-bold justify-center items-center  text-3xl flex  ">{e.text}</p>                 
               </div>
             </section>
           );
         })}
+      </section>
+      <section className="m-auto mt-20 w-full h-full flex flex-col">
+        <h2 className=" py-5 px-7 text-3xl">Soluciones</h2>
+        <article className="w-full flex flex-wrap items-center justify-center">
+        {
+          arrayCarousel.map((e,i)=>{
+            return (
+              <img className="w-44  p-2 h-52" src={e.url} alt="img" />
+            )
+          })
+        }        
+        </article>
       </section>
     </main>
   );
