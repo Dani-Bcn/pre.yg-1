5d39b4e794454449f6dd006dc9f64d11613562c6
Dani-Bcn <nneodani@gmail.com>
2023-12-25 03:27:17 +0100

    test

--- src/Soluciones/catalogos_pdf/Gs_catalogos.jsx
+++ src/Soluciones/catalogos_pdf/Gs_catalogos.jsx
@@ -6,61 +6,61 @@ export default function Gs_catalogos() {
       title:"4G-MIL",
       img: "catalogos/4G-MILL.jpg",
       text: "Para Aceros pre-endurecidos (HRc 20~50) Recubrimiento Y (AlCrN), Carburo ultra nano-grano.",
-      link: "pdf/ye-4g20-4g.pdf",
+      link: "https://ptsc.co.th/catalogfile/yg1/3/YG-1_%5BYE-4G%5D4GMills_Endmill.pdf",
     },
     {
       title:"X-POWER PRO",
       img: "catalogos/X-POWER-PRO.jpg",
       text: "Para Aceros pre-endurecidos (HRc 20~50) Recubrimiento Y (AlCrN), Carburo ultra nano-grano.",
-      link: "pdf/ye-4g20-4g.pdf",
+      link: "https://www.yg1.kr/_include/preview.asp?uploadFile=2019031316035381.pdf&CatalogName=%5BYE%2DXP19%5DX%2Dpower+pro%5F%ED%8E%BC%EC%B9%A8%EB%A9%B4%5F190312%2Epdf",
     },
     {
       title:"K-2",
       img: "catalogos/K-2.jpg",
       text: "Fresas en metal duro integral recubiertas TiAlN para uso general.",
-      link: "pdf/ye-4g20-4g.pdf",
+      link: "https://www.yg1.kr/_include/preview.asp?uploadFile=2020031011101419.PDF&CatalogName=%5BYE%5DK%2D2%5FCARBIDE%5FEND%5FMIILS%2EPDF",
     },
     {
       title:"i-Xmill",
       img: "catalogos/i-Xmill.jpg",
       text: "Programa de copiado 3D para molde y matriz.",
-      link: "pdf/ye-4g20-4g.pdf",
+      link: "https://www.yg1.kr/_include/preview.asp?uploadFile=2019111116463019.pdf&CatalogName=%5BYE%5Di%5FXmill%2Epdf",
     },
     {
       title:"JET-POWER",
       img: "catalogos/JET-POWER.jpg",
       text: "Fresas para el mecanizado de aceros inoxidables y materiales exóticos así como aleaciones con base de Níquel y titanios.",
-      link: "pdf/ye-4g20-4g.pdf",
+      link: "pdf/jetpower.pdf",
     },
     {
       title:"i-SMART",
       img: "catalogos/i-SMART.jpg",
       text: "Programa de fresado modular en rosca métrica.",
-      link: "pdf/ye-4g20-4g.pdf",
+      link: "https://www.yg1.kr/_include/preview.asp?uploadFile=2020010811440797.pdf&CatalogName=%5BYE%5DI%5FSMART%5FEND%5FMILLS%2Epdf",
     },
     {
       title:"ONLY ONE",
       img: "catalogos/ONLY-ONE.jpg",
       text: "FRESAS HSS-PM PARA EL MECANIZADO EN CONDICIONES INESTABLES.",
-      link: "pdf/ye-4g20-4g.pdf",
+      link: "https://www.yg1.kr/_include/preview.asp?uploadFile=2018091216565759.pdf&CatalogName=%5BYE%2DOP16%5DOnlyOne%5F20180711%5F%ED%8E%BC%EC%B9%A8%2Epdf",
     },
     {
       title:"TANK-POWER",
       img: "catalogos/TANK-POWER.jpg",
       text: "Fresas HSS-PM de alta tenacidad para el mecanizado de aceros inoxidables, aceros al carbono y aceros aleados.",
-      link: "pdf/ye-4g20-4g.pdf",
+      link: "pdf/tankpower.pdf",
     },
     {
       title:"HSSCo END MILLS",
       img: "catalogos/HSSCo-END-MILLS.jpg",
       text: "Fresas HSSCo para uso general.",
-      link: "pdf/ye-4g20-4g.pdf",
+      link: "hss-end-mills.pdf",
     },
     {
       title:"MILLING CUTTERS",
       img: "catalogos/MILLING-CUTTERS.jpg",
       text: "Fresas HSSCo al 5% y al 8% para ranurar en T, Woodruff, de disco, de vaso, angulares (cola de Milano).",
-      link: "pdf/ye-4g20-4g.pdf",
+      link: "milling-cutters.pdf",
     },
     {
       title:"ROTARY BURS",
@@ -84,7 +84,7 @@ export default function Gs_catalogos() {
 
       {arrayGsCatalog.map((e, i) => {
         return (
-          <a href="pdf/ye-4g20-4g.pdf" target="_blank"
+          <a href={e.link} target="_blank"
           >
           
 
