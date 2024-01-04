78c6e8f35a979a4af49c40c04f0425ed120bd37c
Dani-Bcn <nneodani@gmail.com>
2024-01-04 14:33:21 +0100

    test

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -37,17 +37,11 @@ export default function Carousel() {
       video: "https://www.youtube.com/watch?v=-ujx_4spMH8",
     },
   ];
-  const colorPlay=[
-    "#FF0000",
-    "#E4FF00",
-    "#00FF27",
-    "#006CFF",
-    "#C500FF"
-  ]
+  const colorPlay = ["#FF0000", "#E4FF00", "#00FF27", "#006CFF", "#C500FF"];
 
   const [count, setCount] = useState(0);
   const [video, setVideo] = useState(false);
-  const [numVideo,setNumVideo] = useState(0)
+  const [numVideo, setNumVideo] = useState(0);
 
   const slider = () => {
     setCount(count + 1);
@@ -56,7 +50,6 @@ export default function Carousel() {
   };
 
   const interval = setInterval(() => slider(), 3000);
-
 
   return (
     <main id="main" className="mt-24 z-20">
@@ -71,7 +64,9 @@ export default function Carousel() {
         </h3>
 
         <button
-          onClick={() => {setVideo(true),setNumVideo(count)}}
+          onClick={() => {
+            setVideo(true), setNumVideo(count);
+          }}
           className="absolute flex justify-center items-center w-16 h-16 rounded-[100px] mt-48 ml-80 bg-slate-50/[0.5] backdrop-blur-sm"
         >
           <svg
@@ -88,27 +83,16 @@ export default function Carousel() {
             <path d="M4 5v14l12 -7z" />
           </svg>
         </button>
-
         {video ? (
-          <div className="fixed flex-col mt-52 -ml-10 md:mt-80 w-[90vw] flex justify-center items-center">
-            <ReactPlayer
-              id="md-movie"
-              className=" bg-slate-800 backdrop-blur-md w-screen x-[150] h-96 border-2 m-auto border-orange-300 z-40 shadow-xl shadow-slate-950"
-              url={arrayCarousel[numVideo].video}
-              playing={video}
-              playsinline={true}
-              muted={false}
-              volume={1}
-              controls={true}
-            ></ReactPlayer>
-            <div
+          <div className="fixed mt-[60vh] h-[150vh] bg-slate-800 backdrop-blur-xl flex flex-col justify-center items-center">
+            <button
               onClick={() => setVideo(false)}
-              className="flex justify-center items-center w-20 h-20 rounded-[100px] mt-5 bg-red-500/[0.5]"
+              className="ml-80 p-10"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="100"
-                height="100"
+                width="30"
+                height="30"
                 viewBox="0 0 24 24"
                 strokeWidth="1.5"
                 stroke="rgb(255,255,255)"
@@ -120,7 +104,19 @@ export default function Carousel() {
                 <path d="M18 6l-12 12" />
                 <path d="M6 6l12 12" />
               </svg>
-            </div>
+            </button>
+            <ReactPlayer
+              id="md-movie"
+              className=" bg-slate-800 backdrop-blur-md "
+              url={arrayCarousel[numVideo].video}
+              width={400}
+              height={300}
+              playing={video}
+              playsinline={true}
+              muted={false}
+              volume={1}
+              controls={true}
+            ></ReactPlayer>
           </div>
         ) : null}
       </div>
